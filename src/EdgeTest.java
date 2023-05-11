583445ed3704919a20717b3499e4c27121078107
Jayath Gunawardena <jayathg1@gmail.com>
2023-05-11 14:45:44 +1000

    Add Test Classes

---
+++ src/EdgeTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class EdgeTest {
+
+    @org.junit.jupiter.api.Test
+    void getDestination() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
